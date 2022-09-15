7251237df6ba8d355770521eb7f8a1de70245ad1
Aamil13 <aamil.shafi13@gmail.com>
2022-09-15 06:50:15 +0530

    2e

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
+
 import Home from './Component/Home';
 import Navb from './Component/Navb';
 import Skills from './Component/Skills';
